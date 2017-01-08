9b4b873e2b2e0b9a12cfaec7216accfbc6ce8df8
dmnpereira@gmail.com <Kwy5|WGYBza$iHJ+ON9p6jeWO,qw!*8C.yqT 0>
2017-01-08 18:59:41 +0100

    Improved JSON response

--- src/main/java/com/koala/tanken/api/rest/FuelStationsResource.java
+++ src/main/java/com/koala/tanken/api/rest/FuelStationsResource.java
@@ -4,14 +4,21 @@ import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.koala.tanken.api.controller.FuelStationsController;
 import com.koala.tanken.api.model.Input;
 import com.koala.tanken.api.util.JacksonConverterException;
 import com.koala.tanken.api.util.JacksonConverterImpl;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class FuelStationsResource implements RequestHandler<Input, String> {
 
     public String handleRequest(Input input, Context context) {
@@ -27,6 +34,24 @@ public class FuelStationsResource implements RequestHandler<Input, String> {
             return null;
         }
 
-		return response.toString();
+        String result = response.toString();
+        //result = StringUtils.remove(result, "\\\"");
+        result = StringUtils.replace(result, "\\\"", "\"");
+        result = StringUtils.replace(result, "\"{", "{");
+        result = StringUtils.replace(result, "}\"", "}");
+        result = StringUtils.replace(result, "\"[", "[");
+        result = StringUtils.replace(result, "]\"", "]");
+
+        ObjectMapper mapper = new ObjectMapper();
+        Object json = null;
+        String indented = result;
+        try {
+            json = mapper.readValue(result, Object.class);
+            indented = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
+        } catch (IOException e) {
+            log.error("Error indenting json response." + e);
+        }
+
+        return indented;
 	}
 }
